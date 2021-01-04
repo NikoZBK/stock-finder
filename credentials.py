14cd6a9b520664a38b86d4a32692a19594357021
NikoZBK <nikozbk@gmail.com>
2021-01-04 12:34:31 -0500

    Update credentials.py
    cleanup

--- credentials.py
+++ credentials.py
@@ -1,53 +1,51 @@
 import os
 from configparser import ConfigParser, NoOptionError
 
-parser = ConfigParser()
-cfg_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'stockfinder' + '.ini')
+try:
+    parser = ConfigParser()
+    cfg_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'stockfinder' + '.ini')
+    parser.read(cfg_file)
+except(FileExistsError, Exception) as e:
+    print('Exception reading settings file: [{}]'.format(e))
 
 
 def get_email():
     try:
-        parser.read(cfg_file)
         return parser.get('CREDENTIALS', 'email')
-    except(ValueError, NoOptionError, Exception) as e:
-        print('Exception retrieving email address: [{}]'.format(e))
+    except(ValueError, NoOptionError, Exception) as e2:
+        print('Exception retrieving email address: [{}]'.format(e2))
 
 
 def get_email_password():
     try:
-        parser.read(cfg_file)
         return parser.get('CREDENTIALS', 'password')
-    except(ValueError, NoOptionError, Exception) as e:
-        print('Exception retrieving email password: [{}]'.format(e))
+    except(ValueError, NoOptionError, Exception) as e2:
+        print('Exception retrieving email password: [{}]'.format(e2))
 
 
 def get_to_number():
     try:
-        parser.read(cfg_file)
         return parser.get('CREDENTIALS', 'to_number')
-    except(ValueError, NoOptionError, Exception) as e:
-        print('Exception retrieving to-number: [{}]'.format(e))
+    except(ValueError, NoOptionError, Exception) as e2:
+        print('Exception retrieving to-number: [{}]'.format(e2))
 
 
 def get_from_number():
     try:
-        parser.read(cfg_file)
         return parser.get('CREDENTIALS', 'from_number')
-    except(ValueError, NoOptionError, Exception) as e:
-        print('Exception retrieving from-number: [{}]'.format(e))
+    except(ValueError, NoOptionError, Exception) as e2:
+        print('Exception retrieving from-number: [{}]'.format(e2))
 
 
 def get_account_sid():
     try:
-        parser.read(cfg_file)
         return parser.get('CREDENTIALS', 'account_sid')
-    except(ValueError, NoOptionError, Exception) as e:
-        print('Exception retrieving account sid: [{}]'.format(e))
+    except(ValueError, NoOptionError, Exception) as e2:
+        print('Exception retrieving account sid: [{}]'.format(e2))
 
 
 def get_auth_token():
     try:
-        parser.read(cfg_file)
         return parser.get('CREDENTIALS', 'auth_token')
-    except(ValueError, NoOptionError, Exception) as e:
-        print('Exception retrieving auth token: [{}]'.format(e))
+    except(ValueError, NoOptionError, Exception) as e2:
+        print('Exception retrieving auth token: [{}]'.format(e2))
